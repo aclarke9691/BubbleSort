2f3c66bd7bf5451ce8e4574977c63e7d2f91b62d
BT_1N3_29@BKTECH.LOCAL <BT_1N3_29@BKTECH.LOCAL>
2019-01-03 14:46:01 -0500

    Initial commit

---
+++ src/Swap.java
@@ -0,0 +1,24 @@
+public class Swap {
+
+    public static void swap(int[]arr,int i,int j)
+    {
+        int temp= arr[j];
+        arr[j]=arr[i];
+        arr[i]= temp;
+
+    }
+
+    public static void bubbleSort(int[] arr)
+    {
+        int swapcount=0;
+        while(swapcount>0)
+        {
+            swapcount=0;
+            for(int i=0; i< arr.length; i++)
+            {
+                if(arr[i]>arr[i+1])
+            }
+
+        }
+    }
+}
