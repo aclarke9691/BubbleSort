b4b3467986e41dc1521d715b8f4d6ea78983d34b
aclarke9691 <aclarke9691@bths.edu>
2019-01-07 06:12:23 -0500

    done

--- src/Swap.java
+++ src/Swap.java
@@ -10,13 +10,17 @@ public class Swap {
 
     public static void bubbleSort(int[] arr)
     {
-        int swapcount=0;
-        while(swapcount>0)
+        boolean swapcount=true;
+        while(swapcount)
         {
-            swapcount=0;
-            for(int i=0; i< arr.length; i++)
+            swapcount=false;
+            for(int i=0; i< arr.length-1; i++)
             {
-                if(arr[i]>arr[i+1])
+                if(arr[i] > arr[i+1])
+                {
+                    swap(arr,i,i+1);
+                    swapcount=true;
+                }
             }
 
         }
